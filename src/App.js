a1cc0d9694a704a2ca1c83763ec47374aad207e8
naidendev <Aiden@gmail.com>
2023-06-06 23:49:28 +0900

    todolist

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Form, Button, ListGroup } from "react-bootstrap";
 
-function App() {
+function TodoList() {
+  const [todoItems, setTodoItems] = useState([]);
+
+  const addTodoItem = (event) => {
+    event.preventDefault();
+    const newTodoItem = {
+      name: event.target.todoItem.value,
+      completed: false,
+    };
+    setTodoItems([...todoItems, newTodoItem]);
+    event.target.todoItem.value = "";
+  };
+
+  const toggleCompleted = (index) => {
+    const updatedTodoItems = [...todoItems];
+    updatedTodoItems[index].completed = !updatedTodoItems[index].completed;
+    setTodoItems(updatedTodoItems);
+  };
+
+  const deleteTodoItem = (index) => {
+    const updatedTodoItems = [...todoItems];
+    if (updatedTodoItems[index].completed === true) {
+      updatedTodoItems.splice(index, 1);
+      setTodoItems(updatedTodoItems);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mt-3">
+      <h1>
+        <string>ToDo List</string>
+      </h1>
+
+      <Form onSubmit={addTodoItem}>
+        <div className="d-flex justify-content-between">
+          <Form.Group controlId="todoItem">
+            <Form.Control type="text" placeholder="Add item" />
+          </Form.Group>
+          <Button variant="primary" type="submit" className="mx-3">
+            Add
+          </Button>
+        </div>
+      </Form>
+      <ListGroup className="mt-3 d-flex justify-content-between">
+        {todoItems.map((todoItem, index) => (
+          <ListGroup.Item
+            key={index}
+            className={`d-flex justify-content-between align-items-center ${
+              todoItem.completed ? "list-group-item-secondary" : ""
+            }`}
+          >
+            <div className="">
+              <Form.Check
+                type="checkbox"
+                checked={todoItem.completed}
+                onChange={() => toggleCompleted(index)}
+                label={todoItem.name}
+                className="mr-auto"
+              />
+            </div>
+            <Button variant="danger" onClick={() => deleteTodoItem(index)}>
+              Delete
+            </Button>
+          </ListGroup.Item>
+        ))}
+      </ListGroup>
     </div>
   );
 }
 
-export default App;
+export default TodoList;
